0f866862b4388acec281edc8ed3602587636a0f5
sivasankar <ssiva1901@outlook.com>
2022-10-16 06:40:16 -0400

    variables created

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {}
+variable "amis" {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable "Main_Routing_Table" {}
+variable "azs" {}
+variable "environment" { default = "dev" }
+variable "instance_type" {}
